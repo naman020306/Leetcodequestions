fe9d8434690c60ad95a001a01b43342454bffa6d
naman020306 <sharmanaman031@gmail.com>
2026-07-06 00:57:03 +0530

    Time: 6 ms (36.67%), Space: 45.4 MB (10.71%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,55 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        //brute force
-      int n = nums1.length;
-      int m = nums2.length;
-      Arrays.sort(nums1);
-      Arrays.sort(nums2);
+//         //brute force
+//       int n = nums1.length;
+//       int m = nums2.length;
+//       Arrays.sort(nums1);
+//       Arrays.sort(nums2);
 
-      ArrayList<Integer> ans = new ArrayList<>();
-      int[] vis = new int[m];
-      for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(nums1[i]==nums2[j] && vis[j]==0){
-                ans.add(nums1[i]);
-                vis[j]=1;
-                break;
-            }
-            if(nums2[j]>nums1[i]){
-                break;
-}}}  
+//       ArrayList<Integer> ans = new ArrayList<>();
+//       int[] vis = new int[m];
+//       for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             if(nums1[i]==nums2[j] && vis[j]==0){
+//                 ans.add(nums1[i]);
+//                 vis[j]=1;
+//                 break;
+//             }
+//             if(nums2[j]>nums1[i]){
+//                 break;
+// }}}  
+                 //optimal solution
+
+       Arrays.sort(nums1);
+       Arrays.sort(nums2);
+       int n = nums1.length;
+       int m = nums2.length;
+
+       int i =0;
+       int j = 0;
+
+       ArrayList<Integer> ans = new ArrayList<>();
+       while(i< n && j < m){
+        if(nums1[i]<nums2[j]){
+            i++;
+        }
+        else if(nums2[j]<nums1[i]){
+            j++;
+
+        }
+        else{
+            ans.add(nums1[i]);
+            i++;
+            j++;
+        }
+       }
+
+
+
+
       int [] result = new int[ans.size()];
-      for(int i = 0; i<ans.size();i++){
-        result[i]=ans.get(i);
+      for(int k = 0; k<ans.size();k++){
+        result[k]=ans.get(k);
 
       }
       return result;
